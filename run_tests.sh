06b514a190c30a4678abb7bc42994a2d803bf50b
kki32 <p107.kitisopakul@gmail.com>
2016-01-19 23:00:51 +0700

    add package.json and run_tests.sh to git

---
+++ run_tests.sh
@@ -0,0 +1,5 @@
+cd /Users/tsn/Desktop/MyMochaChaiSinonExample
+mocha -g 'Code 3' specs --require specs/helpers/chai.js
+mocha -g 'Code 1' specs --require specs/helpers/chai.js
+mocha -g 'Code 2' specs --require specs/helpers/chai.js
+
