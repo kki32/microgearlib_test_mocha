03a1098263ea018bcb1ff273346c363505d03aef
kki32 <p107.kitisopakul@gmail.com>
2016-01-27 14:55:04 +0700

    will change helper file

--- specs/microgear_testing.js
+++ specs/microgear_testing.js
@@ -2696,10 +2696,10 @@ describe('Code 1: Create', function() {
             it('should receive message - starts from unsubscribe', function (done) {
                 this.timeout(itTimeout);
 
-                var stubMessage = sinon.spy();
+                var stubMessage = sinon.stub();
                 microgear.on('message', stubMessage);
 
-                var stubConnect = sinon.spy();
+                var stubConnect = sinon.stub();
                 microgear.on('connected', stubConnect);
 
                 microgear.connect(appid);
@@ -2736,10 +2736,10 @@ describe('Code 1: Create', function() {
             it('should not receive message - starts from subscribe', function (done) {
                 this.timeout(itTimeout);
 
-                var stubMessage = sinon.spy();
+                var stubMessage = sinon.stub();
                 microgear.on('message', stubMessage);
 
-                var stubConnect = sinon.spy();
+                var stubConnect = sinon.stub();
                 microgear.on('connected', stubConnect);
 
                 microgear.connect(appid);
@@ -3344,6 +3344,187 @@ describe('Code 1: Create', function() {
     });
     describe('Code 8: Resettoken', function () {
         ///pre-re: should run helper.js 10 to chat with gearname
+
+        describe('Code 8: Case 1 Resettoken when no cache file', function () {
+            var message;
+            var microgear;
+            var appkey;
+            var appsecret;
+            var appid;
+
+            beforeEach(function () {
+                this.timeout(itTimeout);
+                microgear = undefined;
+                message = "Hello myself.";
+                appkey     = 'NLc1b8a3UZPMhOY';
+                appsecret = 'tLzjQQ6FiGUhOX1LTSjtVKsnSExuX7';
+                appid = 'testNodeJs';
+
+                microgear = MicroGear.create({
+                    key: appkey,
+                    secret: appsecret
+                });
+                //ensure there is no microgear.cache
+                if(fs.existsSync(filePath)){
+                    fs.unlinkSync(filePath);
+                }
+            });
+
+            it('should do nothing', function (done) {
+                microgear.resettoken(function (result) {
+                    if(fs.existsSync(filePath)){
+                        expect('should not create microgear.cache').to.be.false;
+                    }
+                    expect(true).to.be.true;
+                    done();
+                });
+            }, itTimeout);
+        });
+
+        describe('Case 8: Case 2 Resettoken when have microgear.cache and microgear is offline', function () {
+            var message;
+            var gearname;
+            var microgear;
+            var appkey;
+            var appsecret;
+            var appid;
+
+            beforeEach(function (done) {
+                this.timeout(itTimeout);
+                microgear = undefined;
+                gearname = 'myself';
+                message = "Hello myself.";
+                appkey     = 'NLc1b8a3UZPMhOY';
+                appsecret = 'tLzjQQ6FiGUhOX1LTSjtVKsnSExuX7';
+                appid = 'testNodeJs';
+
+                microgear = MicroGear.create({
+                    key: appkey,
+                    secret: appsecret
+                });
+
+                fs.writeFile(filePath, '{"_":null}', function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("clear cache");
+                });
+
+                var stubConnect = sinon.stub();
+                microgear.on('connected', stubConnect);
+
+                var stubDisconnect = sinon.stub();
+                microgear.on('disconnected', stubDisconnect);
+
+                microgear.connect(appid);
+
+                //create state that have microgear.cache but offline
+                setTimeout(function () {
+                    expect(stubConnect.called).to.be.true;
+                    expect(stubConnect.callCount).to.equal(1);
+                    expect(stubDisconnect.notCalled).to.be.true;
+
+                    microgear.disconnect();
+
+                    expect(stubDisconnect.called).to.be.true;
+                    expect(stubDisconnect.callCount).to.equal(1);
+                    done();
+                }, connectTimeout);
+            });
+
+            afterEach(function () {
+                microgear.client.end();
+            });
+
+            it('should clear the cache in resettoken', function (done) {
+                this.timeout(itTimeout);
+                //check the content of microgear cache is not null
+
+                var data = fs.readFileSync(filePath, 'utf8');
+                expect(data.toString()).not.to.equal('{"_":null}');
+
+                //resettoken then check content again
+                microgear.resettoken(function (result) {
+                    var data2 = fs.readFileSync(filePath, 'utf8');
+                    expect(data2.toString()).to.equal('{"_":null}');
+                    done();
+                });
+            });
+        });
+
+        describe('Code 8: Case 3 Resettoken twice', function () {
+            var message;
+            var gearname;
+            var microgear;
+            var appkey;
+            var appsecret;
+            var appid;
+
+            beforeEach(function (done) {
+                this.timeout(itTimeout);
+                microgear = undefined;
+                gearname = 'myself';
+                message = "Hello myself.";
+                appkey     = 'NLc1b8a3UZPMhOY';
+                appsecret = 'tLzjQQ6FiGUhOX1LTSjtVKsnSExuX7';
+                appid = 'testNodeJs';
+
+                microgear = MicroGear.create({
+                    key: appkey,
+                    secret: appsecret
+                });
+
+                fs.writeFile(filePath, '{"_":null}', function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("clear cache");
+                });
+
+                var stubConnect = sinon.stub();
+                microgear.on('connected', stubConnect);
+
+                var stubDisconnect = sinon.stub();
+                microgear.on('disconnected', stubDisconnect);
+
+                microgear.connect(appid);
+
+                setTimeout(function () {
+                    expect(stubConnect.called).to.be.true;
+                    microgear.disconnect();
+                    setTimeout(function () {
+                        expect(stubDisconnect.called).to.be.true;
+                        done();
+                    }, connectTimeout);
+                }, connectTimeout);
+            });
+
+            afterEach(function () {
+                microgear.client.end();
+            });
+
+            it('should just resettoken like usual', function (done) {
+                this.timeout(itTimeout);
+
+                var data = fs.readFileSync(filePath, 'utf8');
+                expect(data.toString()).not.to.equal('{"_":null}');
+
+                microgear.resettoken(function (result) {
+                    var data2 = fs.readFileSync(filePath, 'utf8');
+                    expect(data2.toString()).to.equal('{"_":null}');
+
+                    microgear.resettoken(function (result2) {
+                        var data4 = fs.readFileSync(filePath, 'utf8');
+                        expect(data4.toString()).to.equal('{"_":null}');
+                        done();
+                    });
+                });
+
+            });
+        });
+
+
+
         describe('Code 8: Case 4 Resettoken when offline, after create', function () {
             var message;
             var gearname;
