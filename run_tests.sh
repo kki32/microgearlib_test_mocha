f64f754940f1ab93d2e2628e71718eca9d06a73c
kki32 <p107.kitisopakul@gmail.com>
2016-01-27 16:27:19 +0700

    change bash file

--- run_tests.sh
+++ run_tests.sh
@@ -7,22 +7,22 @@ echo "
 
 
 echo "Code 1 start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-mocha -g 'Code 1: Case 1' specs --require specs/helpers/chai.js
+node_modules/mocha/bin/mocha -g 'Code 1: Case 1' specs --require specs/helpers/chai.js
 sleep 3
 
-mocha -g 'Code 1: Case 2.2' specs --require specs/helpers/chai.js
+node_modules/mocha/bin/mocha -g 'Code 1: Case 2.2' specs --require specs/helpers/chai.js
 sleep 3
 
 node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 10 &
 sleep 3
-mocha -g 'Code 1: Case 4 C' specs --require specs/helpers/chai.js
+node_modules/mocha/bin/mocha -g 'Code 1: Case 4 C' specs --require specs/helpers/chai.js
 sleep 3
 killall node
 sleep 3
 
 node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 19 &
 sleep 3
-mocha -g 'Code 1: Case 4.3' specs --require specs/helpers/chai.js
+node_modules/mocha/bin/mocha -g 'Code 1: Case 4.3' specs --require specs/helpers/chai.js
 sleep 3
 killall node
 sleep 3
@@ -31,7 +31,7 @@ echo "Code 1 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 
 # echo "Code 2 start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# mocha -g 'Code 2' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 2' specs --require specs/helpers/chai.js
 # sleep 3
 # echo "Code 2 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
@@ -39,21 +39,21 @@ echo "Code 1 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # echo "Code 3 start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 10 &
 # sleep 3
-# mocha -g 'Code 3: Case 1, 2, 3 Setalias' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 3: Case 1, 2, 3 Setalias' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 12 &
 # sleep 3
-# mocha -g 'Code 3: Case 4' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 3: Case 4' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 4 &
 # sleep 3
-# mocha -g 'Code 3: Case 5' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 3: Case 5' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
@@ -62,19 +62,19 @@ echo "Code 1 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 
 # echo "Code 4 start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# mocha -g 'Code 4: Case 1' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 1' specs --require specs/helpers/chai.js
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 1 &
 # sleep 3
-# mocha -g 'Code 4: Case 2' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 2' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 6 &
 # sleep 3
-# mocha -g 'Code 4: Case 3' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 3' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
@@ -82,32 +82,32 @@ echo "Code 1 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 7 &
 # sleep 3
-# mocha -g 'Code 4: Case 5' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 5' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
 
-# mocha -g 'Code 4: Case 7' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 7' specs --require specs/helpers/chai.js
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 14 &
 # sleep 3
-# mocha -g 'Code 4: Case 8' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 8' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 11 &
 # sleep 3
-# mocha -g 'Code 4: Case 9' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 9' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 1 &
 # sleep 3
-# mocha -g 'Code 4: Case 10' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 10' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
@@ -115,7 +115,7 @@ echo "Code 1 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 1 &
 # node Users/tsn/Desktop/microgear_helpers/publish_helper/2/publish_helper.js 1 &
 # sleep 3
-# mocha -g 'Code 4: Case 11' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 4: Case 11' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
@@ -127,37 +127,37 @@ echo "Code 1 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # echo "Code 5 start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 4 &
 # sleep 3
-# mocha -g 'Code 5: Case 1' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 5: Case 1' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
-# mocha -g 'Code 5: Case 2' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 5: Case 2' specs --require specs/helpers/chai.js
 # sleep 3
 
-# mocha -g 'Code 5: Case 3' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 5: Case 3' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
-# mocha -g 'Code 5: Case 4' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 5: Case 4' specs --require specs/helpers/chai.js
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 9 &
 # sleep 3
-# mocha -g 'Code 5: Case 5' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 5: Case 5' specs --require specs/helpers/chai.js
 # killall node
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 8 &
 # sleep 3
-# mocha -g 'Code 5: Case 6' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 5: Case 6' specs --require specs/helpers/chai.js
 # killall node
 # sleep 3
 
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 4 &
 # sleep 3
-# mocha -g 'Code 5: Case 7' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 5: Case 7' specs --require specs/helpers/chai.js
 # killall node
 # sleep 3
 
@@ -166,25 +166,25 @@ echo "Code 1 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Code 6 start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 4 &
 # sleep 3
-# mocha -g 'Code 6: Case 1' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 1' specs --require specs/helpers/chai.js
 # sleep 3
 
-# mocha -g 'Code 6: Case 2' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 2' specs --require specs/helpers/chai.js
 # sleep 3
 
-# mocha -g 'Code 6: Case 3' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 3' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
  # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 8 &
-# mocha -g 'Code 6: Case 4' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 4' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
  # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 9 &
-# mocha -g 'Code 6: Case 5' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 5' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
@@ -194,25 +194,25 @@ echo "Code 6 done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Code 7 start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 4 &
 # sleep 3
-# mocha -g 'Code 6: Case 1' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 1' specs --require specs/helpers/chai.js
 # sleep 3
 
-# mocha -g 'Code 6: Case 2' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 2' specs --require specs/helpers/chai.js
 # sleep 3
 
-# mocha -g 'Code 6: Case 3' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 3' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
  # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 8 &
-# mocha -g 'Code 6: Case 4' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 4' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
 
  # node /Users/tsn/Desktop/microgear_helpers/publish_helper/publish_helper.js 9 &
-# mocha -g 'Code 6: Case 5' specs --require specs/helpers/chai.js
+# node_modules/mocha/bin/mocha -g 'Code 6: Case 5' specs --require specs/helpers/chai.js
 # sleep 3
 # killall node
 # sleep 3
